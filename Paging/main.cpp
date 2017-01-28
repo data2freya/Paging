397399592ac9411c46625bdb2f773bcb43c1723a
hayashifreya0 <hayashifreya0@gmail.com>
2017-01-28 16:13:56 -0300

    Initial Commit

---
+++ Paging/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Paging
+//
+//  Created by Freya Hayashi on 1/28/17.
+//  Copyright © 2017 Node2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
